afadbcb8cde3d74b8e737337f1e448aa6ea05e9a
Karthikeyan <yankarto@outlook.com>
2019-02-18 16:05:56 +0530

    springboot with mongodb data hardcode and save then display

--- crud-operations/src/main/java/com/quapt/springboot/topic/Topic.java
+++ crud-operations/src/main/java/com/quapt/springboot/topic/Topic.java
@@ -8,41 +8,48 @@ public class Topic {
 	
 	@Id
 	private String id;
-	private String name;
-	private String description;
+	private String userId;
 	
-	public Topic() {
-		
+	private String title;
+	private String body;
+	private String name;
+	public String getUserId() {
+		return userId;
 	}
-
-	public Topic(String id, String name, String description) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.description = description;
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
-
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public String getDescription() {
-		return description;
+	public Topic(String userId, String id, String title, String body, String name) {
+		super();
+		this.userId = userId;
+		this.id = id;
+		this.title = title;
+		this.body = body;
+		this.name = name;
 	}
-
-	public void setDescription(String description) {
-		this.description = description;
+	
 	}
-}
